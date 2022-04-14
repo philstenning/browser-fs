d4e6fd7f5e06d3adaa88f93d05e6691c3bf56ef3
Phil Stenning <philstenning@hotmail.com>
2022-04-14 11:37:30 +0100

    updateCollection line 23 check if still works Fixes #27

--- packages/fsa-database/src/models/collections/updateCollection.ts
+++ packages/fsa-database/src/models/collections/updateCollection.ts
@@ -4,49 +4,15 @@ import {addFileToCollection} from './addFileToCollection'
 import {removeFileFromCollection} from './removeFileFromCollection'
 
 /**
- * check if there are any differences between the old and new
- * files array and add/remove them as needed. then save the result.
+ * Just updates data, Do not change the files with this function,
+ * it will not add/remove them for you, use the removeFileFromCollection() 
+ * addFileToCollection() instead.
  * @param collection
  * @returns
  */
 export async function updateCollection(collection: fsaCollection) {
-  //get old collection.
-  if (!collection.id) return;
-  const oldCollection = await db.userCollections.get(collection.id);
-  if (!oldCollection) return;
-  const itemsToAdd: fsaCollectionFile[] = [];
-  const itemsToDel: fsaCollectionFile[] = [];
-  // the items are in the new one
-  // but not in the old so need to
-  //be added.
-  // TODO check if this still works
-  collection.files.forEach((n) => {
-    if (oldCollection.files.includes(n)) {
-      itemsToAdd.push(n);
-    }
-  });
-  // the items are in the old one
-  // but have been removed, so we
-  // need to remove them.
-  oldCollection.files.forEach((n) => {
-    if (collection.files.includes(n)) itemsToDel.push(n);
-  });
+ 
 
-  for (const id of itemsToAdd) {
-    const _file = await db.files.get(id);
-    if (_file) {
-      const res = await addFileToCollection(_file, collection);
-      if (res) collection.files = [...collection.files, id];
-    }
-  }
-
-  for (const id of itemsToDel) {
-    const _file = await db.files.get(id);
-    if (_file) {
-      const res = await removeFileFromCollection(collection, _file);
-      if (res) collection.files = [...collection.files.filter((n) => n !== id)];
-    }
-  }
 
   // if any other changes lets save again
   collection.updated = Date.now();
