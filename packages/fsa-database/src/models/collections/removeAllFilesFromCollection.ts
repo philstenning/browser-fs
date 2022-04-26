3d5de1251268ed154b285fe0d33b260db331bdc4
Phil Stenning <philstenning@hotmail.com>
2022-04-26 18:52:55 +0100

    delete all  files from collection saved location Fixes #66

--- packages/fsa-database/src/models/collections/removeAllFilesFromCollection.ts
+++ packages/fsa-database/src/models/collections/removeAllFilesFromCollection.ts
@@ -1,12 +1,14 @@
 import { db } from "../../db/setup";
+import { fsaCollection, fsaFile } from "../types";
+// import { removeFileFromCollectionsSavedLocation } from "./index";
 
-import { removeFileFromCollectionsSavedLocation } from "./index";
 export async function removeAllFilesFromCollection(collectionId: string) {
   const collection = await db.userCollections.get(collectionId);
   if (!collection) return;
 
   const files = await db.files.bulkGet(collection.files.map((f) => f.fileId));
 
+  await removeAllFileFromCollectionsSavedLocation(collection, files);
   // remove collection id from each file
   // then put back in the db
   await db.transaction("rw", db.userCollections, db.files, async () => {
@@ -18,16 +20,33 @@ export async function removeAllFilesFromCollection(collectionId: string) {
         );
         file.userCollectionIds = fileIds;
         await db.files.put(file);
-     
       }
       //
       collection.files = [];
       await db.userCollections.put(collection);
-    
     } catch (err) {
       console.error("error removing files from collection.");
     }
   });
 }
 
+async function removeAllFileFromCollectionsSavedLocation(
+  collection: fsaCollection,
+  files: (fsaFile | undefined)[]
+) {
+  if (!collection.handle) return;
 
+  for (const file of files) {
+    if (file) {
+      try {
+        
+        await collection.handle.removeEntry(file.name);
+      } catch (e) {
+        console.warn(`unable to remove file from local drive it probably did not exist, 
+         directory: ${collection.handle.name} 
+         file: ${file.name} 
+         message: ${e}`);
+      }
+    }
+  }
+}
