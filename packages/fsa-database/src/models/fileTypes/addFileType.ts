bdcb5a2063dbee116ddeb82d50eea3380435b768
Phil Stenning <philstenning@hotmail.com>
2022-05-17 10:56:05 +0100

    prevent multiple file extensions having the same name Fixes #135

--- packages/fsa-database/src/models/fileTypes/addFileType.ts
+++ packages/fsa-database/src/models/fileTypes/addFileType.ts
@@ -10,6 +10,10 @@ import { fsaFileType } from '../types'
 export default async function addFileType(name: string): Promise<number | boolean> {
   if (!name) return false
   const formattedName = name.replace('.', '').trim().toLowerCase()
+  
+  // prevent same ext being added again
+   if (await fileTypeExists(formattedName)) return false
+
   const fileType: fsaFileType = {
     name: formattedName,
     hidden: false,
@@ -24,6 +28,17 @@ export default async function addFileType(name: string): Promise<number | boolea
     return false
   } catch (error) {
     console.error(`Error saving fileType ${error}`)
+  }
+  return false
+}
+
+
+async function fileTypeExists(name:string){
+  try{
+    const count =await db.fileTypes.where({name}).count()
+    if(!!count) return true
+  }catch(error){
+    console.error(`Error getting fileType: ${error}`);
   }
   return false
 }
