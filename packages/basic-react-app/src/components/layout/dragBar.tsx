6b424875b35ad1594692891162740bfa38a456e0
Phil Stenning <philstenning@hotmail.com>
2022-05-29 17:22:52 +0100

    drag

--- packages/basic-react-app/src/components/layout/dragBar.tsx
+++ packages/basic-react-app/src/components/layout/dragBar.tsx
@@ -1,20 +1,43 @@
+import {useState} from 'react'
 import styles from './dragBar.module.css'
 
 const DragBar = () => {
+  const [isDragging,setIsDragging ] = useState(false)
+  const [clientX,setClientX] = useState(200)
 
-  // const style = {"--left-sidebar:15vw"} as React.CSSProperties
+  const sidePanel = (e: React.MouseEvent<HTMLDivElement>) => {
+    // if(e.clientX>0){
+    //   document.documentElement.style.setProperty('--left-sidebar', `${e.clientX}px`)
+    // }
+    // console.log('end')
+    if (e.clientX > 0) {
+      console.log(e.clientX)
+      document.documentElement.style.setProperty('--left-sidebar', `${e.clientX}px`)
+      // console.log(e.clientX)
+      // document.documentElement.style.setProperty('--left-sidebar', `${e.clientX}px`)
+      // setClientX(e.movementX)
+      // setClientX(e.movementX)
+      // console.log(e.movementX, e.clientX)
+    }
+  }
+
   return (
     // @ts-ignore
     <div
       // style={{ '--left-sidebar': '12vw' } as React.CSSProperties}
       className={styles.bar}
-      // onDragStart={(e)=>console.log(e.clientX)}
-      // onDrag={(e) => console.log('dr', e.clientX)}
+      // style={{transform:`translateX(${clientX}px)`}}
+      onDrag={(e) => sidePanel(e)}
+      onDragStart={e=>e.dataTransfer.setData('data','')}
+      // // onDrag={(e) => console.log('dr', e.clientX)}
       draggable
-      onDragEnd={(e) => console.log('end', e.clientX)}
-      // onMouseDown={(e)=>console.log([e.clientX])}
+      onDragEnd={(e) => sidePanel(e)}
+      // onMouseDown={(e) => setIsDragging(true)}
+      // onMouseUp={(e) => setIsDragging(false)}
+      // onMouseMove={sidePanel}
+      // onMouseLeave={()=> setIsDragging(false)}
     >
-      {' '}
+      {isDragging.toString()}
     </div>
   )
 }
--- packages/web/postcss.config.js
+++ packages/web/postcss.config.js
@@ -1,16 +1,16 @@
-const postcssJitProps = require("postcss-jit-props");
-const OpenProps = require("open-props");
-const postcssNesting = require("postcss-nesting");
-const postcssImport = require("postcss-import");
-const combineSelectors = require("postcss-combine-duplicated-selectors");
+const postcssJitProps = require('postcss-jit-props')
+const OpenProps = require('open-props')
+const postcssNesting = require('postcss-nesting')
+const postcssImport = require('postcss-import')
+const combineSelectors = require('postcss-combine-duplicated-selectors')
 // const cssnano = require("cssnano");
-const postcssSorting = require("postcss-sorting");
-const customMedia = require("postcss-custom-media");
-// const importGlob = require("postcss-import-ext-glob");
-
+const postcssSorting = require('postcss-sorting')
+const customMedia = require('postcss-custom-media')
+const importGlob = require('postcss-import-ext-glob')
 
 module.exports = {
   plugins: [
+    importGlob(),
     postcssImport(),
     postcssJitProps(OpenProps),
     customMedia(),
