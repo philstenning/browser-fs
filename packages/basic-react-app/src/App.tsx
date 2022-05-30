03e21f4e9da9cb31b55881b187bdeaea2f8df8e9
Phil Stenning <philstenning@hotmail.com>
2022-05-30 19:28:46 +0100

    wip

--- packages/basic-react-app/src/App.tsx
+++ packages/basic-react-app/src/App.tsx
@@ -1,39 +1,124 @@
 import { FsaDbContextProvider } from 'react-fsa-database'
+import { useState } from 'react'
 // import RootDirectories from './components/rootDirectories'
 // import Directories from './components/directories'
 
 // import Collections from './components/collections'
-import LeftSidebar from './components/layout/leftSidebar'
-import RightSidebar from './components/layout/rightSidebar'
-import Display from './components/display'
+// import LeftSidebar from './components/layout/leftSidebar'
+// import RightSidebar from './components/layout/rightSidebar'
+// import Display from './components/display'
 
-import DragBar from './components/layout/dragBar'
+// import DragBar from './components/layout/dragBar'
 import './app.css'
 import Grid from './components/layout/grid'
-import Layout from './components/layout/layout'
+// import Layout from './components/layout/layout'
 function App() {
+  const [leftPanelIsCollapsed, setLeftPanelIsCollapsed] = useState(true)
+  const [rightPanelIsCollapsed, setRightPanelIsCollapsed] = useState(true)
   return (
     <div className="app">
       <FsaDbContextProvider
-        fileExtensionsForApp={['gcode', '.3mf', 'jpg', 'stl']}>
-       {/* <Layout>
+        fileExtensionsForApp={['gcode', '.3mf', 'jpg', 'stl']}
+      >
+        {/* <Layout>
          <LeftSidebar/>
          <DragBar/>
          <Display/>
          <RightSidebar/>
        </Layout> */}
-       <Grid>
-         <div>one</div>
-         <div>two</div>
-         <div>three</div>
-       </Grid>
-
-  
-        
-       
+        <div className="spacer">
+          <div className="btn-group">
+         
+            <button
+              onClick={() => setLeftPanelIsCollapsed((current) => !current)}
+            >
+              collapse Left{' '}
+            </button>
+            <button
+              onClick={() => setRightPanelIsCollapsed((current) => !current)}
+            >
+              collapse Right{' '}
+            </button>
+          </div>
+          <Grid collapseRight={rightPanelIsCollapsed} collapseLeft={leftPanelIsCollapsed}>
+            <div style={{overflow:'hidden'}}>
+              {' '}
+              Lorem {leftPanelIsCollapsed.toString()}{' '}
+              {rightPanelIsCollapsed.toString()} ipsum dolor sit amet,
+              consectetur adipisicing elit. Quam at eveniet provident eos
+              voluptatibus nemo voluptates et error? Laborum at quibusdam
+              expedita sint iste, nobis nulla debitis enim, magnam molestias
+              quod a incidunt ipsum obcaecati ad, error architecto earum rem sit
+              vero quis voluptatem! Possimus fugiat earum officia pariatur
+              numquam accusamus itaque soluta magni hic veritatis ea facere id
+              ullam, corporis sequi saepe quia quasi incidunt adipisci
+              temporibus culpa dolores nesciunt animi iste. Aliquid nobis
+              nostrum corporis ex amet iure porro sit asperiores dolor. Facere
+              sint rerum qui quod sunt, earum commodi similique perspiciatis,
+              porro cupiditate in dolore. Nostrum, a!
+            </div>
+            <Cards />
+            <div style={{height:'100%',overflow:'auto'}}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quo nihil dignissimos numquam voluptates ratione
+             s deserunt, similique aperiam?
+            </div>
+          </Grid>
+        </div>
       </FsaDbContextProvider>
     </div>
   )
 }
 
 export default App
+
+
+const Cards = ()=>{
+  const cards = [1,2,3,4,5,6,7,8,9,10]
+  return (
+    <div className='card-wrapper'>
+
+    <div className='cards'>
+      {cards.map(card=>(
+        <div key={card} className='card'>
+           {card}
+        </div>
+      ))}
+    </div>
+      </div>
+  )
+}
+
--- packages/basic-react-app/src/components/layout/grid.tsx
+++ packages/basic-react-app/src/components/layout/grid.tsx
@@ -1,27 +1,78 @@
-import { useState, CSSProperties } from 'react'
+import { useState, CSSProperties, useRef, useEffect } from 'react'
 
 import styles from './grid.module.css'
 
 type ResizableGrid = {
   children: React.ReactNode[]
+  minWidth?: number
+  collapseLeft?: boolean
+  collapseRight?: boolean
 }
 
-function Grid({ children }: ResizableGrid) {
-  const [leftPanel, setLeftPanel] = useState(250)
-  const [rightPanel, setRightPanel] = useState(250)
+function Grid({
+  children,
+  collapseLeft = true,
+  collapseRight = true,
+  minWidth = 200,
+}: ResizableGrid) {
+  const [panelWidths, setPanelWidths] = useState([200, collapseRight ? 0 : 200])
   const [currentPanel, setCurrentPanel] = useState(0)
   const [isResizing, setIsResizing] = useState(false)
-
+  const gridRef = useRef<HTMLDivElement>(null)
   const handleResize = (isResizing: boolean, currentPanel: number) => {
+    // console.log(isResizing, currentPanel)
     setCurrentPanel(currentPanel)
     setIsResizing(isResizing)
   }
+
+  useEffect(() => {
+    if (collapseRight) {
+      setPanelWidths((current) => [current[0], 0])
+    } else {
+      setPanelWidths((current) => [current[0], minWidth])
+    }
+  }, [collapseRight])
+
+  useEffect(() => {
+    if (collapseLeft) {
+      setPanelWidths((current) => [0, current[1]])
+    } else {
+      setPanelWidths((current) => [minWidth, current[1]])
+    }
+  }, [collapseLeft])
 
   const resize = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation()
     e.preventDefault()
     if (isResizing) {
-      setLeftPanel(e.clientX)
+      // setLeftPanel(e.clientX)
+      const gridWidth = gridRef.current?.clientWidth ?? 0
+      const leftOffset = gridRef.current?.offsetLeft ?? 0
+      const mousePosition = e.clientX
+      const maxRight = gridWidth + leftOffset
+
+      if (currentPanel === 0) {
+        let newVal = mousePosition - leftOffset
+        if (collapseLeft) newVal = 0
+        if (
+          mousePosition >= leftOffset + minWidth &&
+          mousePosition <= maxRight - (minWidth + panelWidths[1])
+        ) {
+          setPanelWidths((current) => [newVal, current[1]])
+        }
+      } else {
+        // right hand panel
+        let newVal = gridWidth - (mousePosition - leftOffset)
+        if (newVal < minWidth) newVal = minWidth
+        if (collapseRight) newVal = 0
+        setPanelWidths((current) => [current[0], newVal])
+      }
+      // console.log(
+      //   { gridWidth },
+      //   { leftOffset },
+      //   { mousePosition },
+      //   { maxRight }
+      // )
     }
   }
 
@@ -29,31 +80,65 @@ function Grid({ children }: ResizableGrid) {
     e.stopPropagation()
     e.preventDefault()
     if (isResizing) {
-      setLeftPanel(e.clientX)
+      // setLeftPanel(e.clientX)
+      setIsResizing(false)
+    }
+  }
+
+  const handleLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const gridWidth = gridRef.current?.clientWidth ?? 0
+    const offset = gridRef.current?.offsetLeft ?? 0
+    const mousePosition = e.clientX
+    const zero = offset - mousePosition
+
+    if (mousePosition === zero || mousePosition >= gridWidth + offset) {
       setIsResizing(false)
     }
   }
 
   return (
     <div
+      ref={gridRef}
       className={styles.container}
       style={
         {
-          '--left-panel': `${leftPanel}px`,
-          '--right-panel': `${rightPanel}px`,
+          gridTemplateColumns: `${panelWidths[0]}px ${
+            collapseLeft ? '0' : 'calc(0.5rem + 5px)'
+          } 1fr ${collapseRight ? '0' : 'calc(0.5rem + 5px)'} ${
+            panelWidths[1]
+          }px`,
         } as CSSProperties
       }
+      // style={
+      //   {
+      //     '--left-panel': `${panelWidths[0]}px`,
+      //     '--right-panel': `${panelWidths[1]}px`,
+      //   } as CSSProperties
+      // }
       onMouseMove={resize}
       onMouseUp={resizeFinish}
+      onMouseLeave={handleLeave}
     >
-      {children.map((child, index) => (
+      {children[0]}
+      <Divider handleResize={handleResize} id={0} isCollapsed={collapseLeft} />
+      {children.length >= 2 && children[1]}
+
+      {children.length >= 3 && (
+        <Divider
+          handleResize={handleResize}
+          isCollapsed={collapseRight}
+          id={1}
+        />
+      )}
+      {children.length >= 3 && children[2]}
+      {/* {children.map((child, index) => (
         <>
           {child}
           {children.length - index > 1 && (
-            <Divider setIsResizing={setIsResizing} id={1} />
+            <Divider handleResize={handleResize} id={index} />
           )}
         </>
-      ))}
+      ))} */}
     </div>
   )
 }
@@ -62,21 +147,22 @@ export default Grid
 
 type Props = {
   id: number
-  // setIsResizing: React.Dispatch<React.SetStateAction<boolean>>
-  handleResize:()=>void
+  isCollapsed: boolean
+  handleResize: (isResizing: boolean, currentPanel: number) => void
 }
 
-const Divider = ({ setIsResizing, id }: Props) => {
+const Divider = ({ handleResize, id, isCollapsed }: Props) => {
   const handleMouseEvent = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     e.stopPropagation()
     e.preventDefault()
-    setIsResizing(true)
+    handleResize(true, id)
   }
   return (
-    <div onMouseDown={handleMouseEvent} className={styles.divider}>
-      {id.toString()}
-    </div>
+    <div
+      onMouseDown={handleMouseEvent}
+      className={isCollapsed ? '' : styles.divider}
+    ></div>
   )
 }
