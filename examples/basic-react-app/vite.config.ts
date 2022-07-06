2275f831d41262944af58e6b195320f0c1090ac0
Phil Stenning <philstenning@hotmail.com>
2022-07-06 10:49:21 +0100

    build(Library all): update build config

--- examples/basic-react-app/vite.config.ts
+++ examples/basic-react-app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],server:{port:8842}
+  plugins: [react()],
+  server: { port: 8842 },
 })
