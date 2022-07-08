fee2c4a6986c458579d34a6a5eb6ee77b9ee3723
Phil Stenning <philstenning@hotmail.com>
2022-07-08 13:44:15 +0100

    docs(docs): 🐛 fix react router now has correct basename

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  if (mode === 'production') {
-    return {
-      plugins: [react()],
-      base: '/browser-fs/'
-    }
-  }
+   if (mode === 'production') {
+     return {
+       plugins: [react()],
+      //  base: '/browser-fs/'
+     }
+   }
   return { plugins: [react()] }
 })
