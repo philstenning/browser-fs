b060ea3b28a674038fc911fbdfe23c22d1340179
Phil Stenning <philstenning@hotmail.com>
2022-07-08 12:06:44 +0100

    docs(docs): 🐛 add base path to vite config  to fix links not resolving

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  if (mode === 'production') {
+    return {
+      plugins: [react()],
+      base: '/browser-fs/'
+    }
+  }
+  return { plugins: [react()] }
 })
