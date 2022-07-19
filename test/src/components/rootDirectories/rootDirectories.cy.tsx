04e6aa3fd00dc40412501f67947bea10d1e87029
Phil Stenning <philstenning@hotmail.com>
2022-07-19 09:35:02 +0100

    test(test): run all tests in cypress at once

--- test/src/components/rootDirectories/rootDirectories.cy.tsx
+++ test/src/components/rootDirectories/rootDirectories.cy.tsx
@@ -1,9 +1,9 @@
-
-import RootDirs from './rootDirectories'
 import { FsaDbContextProvider } from 'react-fsa-database'
 describe('<RootDirs>', () => {
   it('mount', () => {
     cy.mount(
+      // this won't work atm the provider needs dexie 
+      // in the indexDB  and this doesn't load atm??
       <FsaDbContextProvider>
 
         <div>fff</div>
