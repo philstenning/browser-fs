30a0a9d5f96f3961136a8c750f1d9db81db12172
Phil Stenning <philstenning@hotmail.com>
2022-07-26 09:50:16 +0100

    update next to latest version

--- examples/docs/pages/index.tsx
+++ examples/docs/pages/index.tsx
@@ -1,23 +1,13 @@
 import type { NextPage } from 'next'
-import {
-  ResizableHorizontalGrid,
-  ResizableVerticalGrid
-} from 'react-resizable-collapsible-grid'
-
 // import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-// import Layout from '../components/layout/layout'
 
-import  'react-resizable-collapsible-grid/dist/index.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-     <ResizableHorizontalGrid >
-       <div>ok</div>
-       <div>ok</div>
-       <div>ok</div>
-     </ResizableHorizontalGrid>
+ <h1>Browser fs</h1>
+ <h3>Lorem, ipsum dolor sit Dignissimos, voluptatibus.</h3>
     </div>
   )
 }
