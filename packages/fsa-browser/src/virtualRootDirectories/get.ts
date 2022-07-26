300d1ffb72b292ee965406c659c9db5219ea04ae
Phil Stenning <philstenning@hotmail.com>
2022-07-26 12:07:07 +0100

    tidy file for docs

--- packages/fsa-browser/src/virtualRootDirectories/get.ts
+++ packages/fsa-browser/src/virtualRootDirectories/get.ts
@@ -1,70 +1,68 @@
-import { entries } from "idb-keyval";
-import { VirtualRootDirectoryType } from "./types";
-import { checkPermissionsOfHandle } from "../fileSystemOperations";
-import { rootStore } from "./stores";
+import { entries } from 'idb-keyval'
+import { VirtualRootDirectoryType } from './types'
+import { checkPermissionsOfHandle } from '../fileSystemOperations'
+import { rootStore } from './stores'
 
 /**
  *  All VirtualFileSystemEntries that have been saved to indexDB are returned
  * @param prependedText  The text you you have prepend all your entries with. Default is empty string.
  */
-async function getAllVirtualRootDirectories(prependedText: string = "") {
+async function getAllVirtualRootDirectories(prependedText: string = '') {
   try {
-    const allEntries = await entries(rootStore);
+    const allEntries = await entries(rootStore)
 
-    const allFolders: VirtualRootDirectoryType[] = [];
+    const allFolders: VirtualRootDirectoryType[] = []
     allEntries.forEach((entry) => {
       if (entry[0].toLocaleString().startsWith(prependedText)) {
         // update the permissions to false and add it to our results.
-        const folder = entry[1] as VirtualRootDirectoryType;
-        folder.hasReadPermission = false;
-        allFolders.push(folder);
+        const folder = entry[1] as VirtualRootDirectoryType
+        folder.hasReadPermission = false
+        allFolders.push(folder)
       }
-    });
+    })
     // console.table(allFolders);
-    const res = orderDirectoriesByDate(allFolders);
-    return res;
+    const res = orderDirectoriesByDate(allFolders)
+    return res
   } catch (e) {
-    console.error("error getting handles:", e);
+    console.error('error getting handles:', e)
   }
-  return null;
+  return null
 }
-
-type Order = "asc" | "desc";
 
 function orderDirectoriesByDate(
   directories: VirtualRootDirectoryType[],
-  order: Order = "asc"
+  order: 'asc' | 'desc' = 'asc'
 ) {
   return directories.sort((a, b) => {
-    if (order === "asc") {
-      return a.created < b.created ? -1 : 1;
+    if (order === 'asc') {
+      return a.created < b.created ? -1 : 1
     }
-    return a.created > b.created ? -1 : 1;
-  });
+    return a.created > b.created ? -1 : 1
+  })
 }
 
 async function getAllVirtualRootDirectoriesAndCheckPermissions(
-  mode: FileSystemPermissionMode = "read"
+  mode: FileSystemPermissionMode = 'read'
 ) {
-  const virtualFileSystemHandles = await getAllVirtualRootDirectories();
-  if (!virtualFileSystemHandles) return;
+  const virtualFileSystemHandles = await getAllVirtualRootDirectories()
+  if (!virtualFileSystemHandles) return
 
-  const checkedHandles: VirtualRootDirectoryType[] = [];
+  const checkedHandles: VirtualRootDirectoryType[] = []
 
   for (const virtualFH of virtualFileSystemHandles) {
     virtualFH.hasReadPermission = await checkPermissionsOfHandle(
       virtualFH.handle as FileSystemDirectoryHandle,
       mode
-    );
-    checkedHandles.push(virtualFH);
+    )
+    checkedHandles.push(virtualFH)
   }
-  console.log({ checkedHandles });
+  console.log({ checkedHandles })
 
-  return checkedHandles;
+  return checkedHandles
 }
 
 export {
   getAllVirtualRootDirectories,
   getAllVirtualRootDirectoriesAndCheckPermissions,
-  orderDirectoriesByDate,
-};
+  orderDirectoriesByDate
+}
